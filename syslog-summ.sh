5ab29fe1e9c07367a3419642272a19c94145a888
J <3143482+jclingensmith@users.noreply.github.com>
2023-01-25 06:35:55 -0500

    Delete syslog-summ.sh
    added to one-liners.txt

--- syslog-summ.sh
+++
@@ -1,2 +0,0 @@
-grep -E '(warning|error|critical)' syslog.txt | awk '{print $1 " " $2 " " $3 " " $6}' | sort | uniq -c > summary.txt
-#awk '/(warning|error|critical)/ {print $1 " " $2 " " $3 " " $6}' syslog.txt | sort | uniq -c > summary.txt
