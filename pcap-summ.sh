84966fb2a06b3c7e4f421057c4eef03c9a8e9e2e
J <3143482+jclingensmith@users.noreply.github.com>
2023-01-25 06:35:40 -0500

    Delete pcap-summ.sh
    added to one-liners.txt

--- pcap-summ.sh
+++
@@ -1,2 +0,0 @@
-tshark -r capture.pcap -T fields -e ip.src -e ip.dst -e frame.protocols -e ssl.handshake.ciphersuite -Y "ssl" -E separator=, -E header=y > summary.csv
-#tcpdump -nn -r capture.pcap | awk '{print $3, $5, $9}' | sort | uniq -c > summary.txt
