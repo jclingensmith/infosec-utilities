1bd133486f2122ceb25856e956b3da49fbcc7aab
J <3143482+jclingensmith@users.noreply.github.com>
2023-01-25 06:27:00 -0500

    Update syslog-summ.sh
    added awk alternative

--- syslog-summ.sh
+++ syslog-summ.sh
@@ -1,1 +1,2 @@
 grep -E '(warning|error|critical)' syslog.txt | awk '{print $1 " " $2 " " $3 " " $6}' | sort | uniq -c > summary.txt
+#awk '/(warning|error|critical)/ {print $1 " " $2 " " $3 " " $6}' syslog.txt | sort | uniq -c > summary.txt
