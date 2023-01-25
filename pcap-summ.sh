bad06251c07b0c9f38fc9ec9af00d3e1ecbe8a1b
J <3143482+jclingensmith@users.noreply.github.com>
2023-01-25 06:23:48 -0500

    Update pcap-summ.sh
    added tcpdump alternative for reference

--- pcap-summ.sh
+++ pcap-summ.sh
@@ -1,1 +1,2 @@
 tshark -r capture.pcap -T fields -e ip.src -e ip.dst -e frame.protocols -e ssl.handshake.ciphersuite -Y "ssl" -E separator=, -E header=y > summary.csv
+#tcpdump -nn -r capture.pcap | awk '{print $3, $5, $9}' | sort | uniq -c > summary.txt
